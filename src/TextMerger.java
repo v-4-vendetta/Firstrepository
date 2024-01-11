008636b86d00cc6d5419fd08377b673643531363
mahir <mahir.nazli@gmail.com>
2024-01-11 03:07:41 +0300

    PlanetClass implemented

--- src/TextMerger.java
+++ src/TextMerger.java
@@ -2,11 +2,23 @@ import java.util.HashMap;
 
 public class TextMerger {
     public static void main(String[] args) {
-        String[] metinler = new String[2];
+        String[] metinler = {"book","music","car","store","market","bottle"};
+        StringBuilder result = new StringBuilder();
 
-        for (int i=0; i<metinler.length; i++)
-        {
+        for (int i = 0; i < metinler.length; i++) {
+
+            for (int j = 0; j < metinler[i].length(); j++) {
+                for (int k=j+1; k < metinler[i].length()-1; k++) {
+                    if (metinler[i].charAt(j) == metinler[i].charAt(k)) {
+                        result.append(metinler[i] + " ");
+                        break;
+                    }
+                }
+            }
 
         }
+
+        System.out.println("Repeated characters text : " + result);
+
     }
 }
